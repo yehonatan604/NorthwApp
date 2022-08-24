3b1890bab795c1d6504436d721e600299502f990
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2022-08-24 23:13:37 +0300

    Add project files.

---
+++ NorthwApp.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using NorthwApp.Entities.Models;
+
+namespace NorthwApp.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly NorthwindContext db = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dgrid.Visibility = Visibility.Collapsed;
+
+            Style style = this.FindResource("BtnStyle") as Style;
+
+            foreach (Button button in Buttons.Children)
+            {
+                button.Style  = style;
+            }
+
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from prd in db.Products
+                        select new {prd.ProductName, prd.QuantityPerUnit} ;
+
+            FillGrid(query);
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from prd in db.Products
+                        where prd.Discontinued == false 
+                        select new { prd.ProductId, prd.ProductName };
+
+            FillGrid(query);
+        }
+
+        private void Btn3_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from prd in db.Products
+                        where prd.Discontinued == true
+                        select new { prd.ProductId, prd.ProductName };
+
+            FillGrid(query);
+        }
+
+        private void Btn4_Click(object sender, RoutedEventArgs e)
+        {
+            int dis = (from prd in db.Products
+                       where prd.Discontinued == false
+                       select prd.Discontinued).Count();
+
+            int cur = (from prd in db.Products
+                       where prd.Discontinued == true
+                       select prd.Discontinued).Count();
+
+            var query = from i in new List<int>() { 890 }
+                        select new { discontinued = dis, continued = cur };
+
+            FillGrid(query);
+        }
+
+        private void FillGrid(IEnumerable<object> query)
+        {
+            Dgrid.ItemsSource = query.ToList();
+            Dgrid.Visibility = Visibility.Visible;
+        }
+    }
+}
